f2b57399daae162d290517f1ce0fe6d12ae12bdd
Егор Бойко <ego.boiko2018@yandex.ru>
2023-01-20 14:28:04 +0300

    Update urls.py

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# Главная страница
+def index(request):   
+    return HttpResponse('Главная страница')
+
+
+# Страница с информацией об одном сорте мороженого;
+# view-функция принимает параметр pk из path()
+def group_posts(request, slug):
+    return HttpResponse(f'Посты по группам {slug}')
